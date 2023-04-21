2f43cfc4fb07c0bec4f3328f4172ac7efe779e84
DawsonBolen <dawson120@live.com>
2023-04-21 15:08:03 -0700

    got it mostly working just need to add the date at the top and finish the readme

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,90 @@
+
+$(document).ready(function () {
+  var eventsData;
+
+  function setHourColors() {
+    var now = dayjs();
+
+    for(var i = 9; i < 17; i++) {
+       // $("#hour-" + i + " textarea").removeClass("future");
+        if(i < now.hour()){
+            $("#hour-" + i + " textarea").addClass("past");
+        } else if(i == now.hour()) {
+            $("#hour-" + i + " textarea").addClass("present");
+        } else {
+            $("#hour-" + i + " textarea").addClass("future");
+        }
+    }
+  }
+ 
+  function loadStoredData() {
+    eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
+    if (!eventsData) {
+      eventsData = {
+        hour9: "",
+        hour10: "",
+        hour11: "",
+        hour12: "",
+        hour13: "",
+        hour14: "",
+        hour15: "",
+        hour16: "",
+        hour17: ""
+      }
+    }
+
+    for(let key in eventsData) {
+      const id = "hour-" + key.slice(4);
+      const div = $('#' + id);
+      div.children('textarea').val(eventsData[key]);
+    }
+
+  }
+
+  $('.saveBtn').on('click', handleSaveClick);
+
+  function handleSaveClick(event) {
+    var hourBlock = $(event.target).parent();
+    var value = hourBlock.children("textarea").val();
+    var hour = hourBlock.attr('id').split("-")[1];
+    
+    //modify data object
+
+    eventsData["hour" + hour] = value;
+
+    localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
+
+
+  }
+
+    
+
+    $(function() {
+      loadStoredData();
+      setHourColors();
+    });
+
+
+});
+
+
+
+
+//GIVEN I am using a daily planner to create a schedule
+//WHEN I open the planner
+//THEN the current day is displayed at the top of the calendar
+//WHEN I scroll down
+//THEN I am presented with timeblocks for standard business hours
+//WHEN I view the timeblocks for that day
+//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+//WHEN I click into a timeblock
+//THEN I can enter an event
+//WHEN I click the save button for that timeblock
+//THEN the text for that event is saved in local storage
+//WHEN I refresh the page
+//THEN the saved events persist
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -23,62 +110,3 @@
    
     
     // TODO: Add code to display the current date in the header of the page.
- 
-$(document).ready(function () {
-  var eventsData;
-
-  function setHourColors() {
-    var now = dayjs();
-
-    for(var i = 9; i < 18; i++) {
-       // $("#hour-" + i + " textarea").removeClass("future");
-        if(i < now.hour()){
-            $("#hour-" + i + " textarea").addClass("past");
-        } else if(i == now.hour()) {
-            $("#hour-" + i + " textarea").addClass("present");
-        } else {
-            $("#hour-" + i + " textarea").addClass("future");
-        }
-    }
-  }
- 
-  function loadStoredData() {
-    var eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
-
-    if (!eventsData) {
-      eventsData = {
-        hour9: "",
-        hour10: "",
-        hour11: ""
-      }
-    }
-  }
-
-    function handleSaveClick(event) {
-        var hourBlock = $(event.target).parent();
-        var value = hourBlock.children("textarea").val();
-        var hour = hourBlock.attr('id').split("-")[1];
-
-        //modify data object
-
-        eventsData["hour" + hour] = value;
-
-        localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
-
-
-      }
-
-      $('.saveBtn').on('click', handleSaveClick);
-
-      $(function() {
-        loadStoredData();
-        setHourColors();
-      });
-
-
-
-
-
-
-});
-
